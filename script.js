71cf7a95dc5ac0d91c1669cf7d2f3eb836d036e9
stmacoli <st.macoli@gmail.com>
2026-03-18 01:01:26 -0300

    hero-servicos-sobre_empresa

--- script.js
+++ script.js
@@ -10,31 +10,7 @@ const observer = new IntersectionObserver((entries) => {
 
 elementos.forEach((el) => observer.observe(el));
 
-function animarNumero(id, alvo, sufixo, duracao = 1500) {
-  const elemento = document.getElementById(id);
-  let inicio = 0;
-  const incremento = alvo / (duracao / 16);
 
-  function atualizar() {
-    inicio += incremento;
-
-    if (inicio < alvo) {
-      elemento.textContent = Math.floor(inicio) + sufixo;
-      requestAnimationFrame(atualizar);
-    } else {
-      elemento.textContent = alvo + sufixo;
-    }
-  }
-
-  atualizar();
-}
-
-window.addEventListener('load', () => {
-  animarNumero('num1', 40, '%');
-  animarNumero('num2', 12, 'h');
-  animarNumero('num3', 60, '%');
-  animarNumero('num4', 90, '%');
-});
 
 const form = document.getElementById('contactForm');
 const statusMensagem = document.getElementById('statusMensagem');
