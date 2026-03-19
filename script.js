fa1a377b02330217f8b15f41cc3600752af9ece9
stmacoli <st.macoli@gmail.com>
2026-03-19 19:12:00 -0300

    formulario-envio

--- script.js
+++ script.js
@@ -12,13 +12,3 @@ elementos.forEach((el) => observer.observe(el));
 
 
 
-const form = document.getElementById('contactForm');
-const statusMensagem = document.getElementById('statusMensagem');
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const nome = document.getElementById('nome').value;
-  statusMensagem.innerHTML = `Obrigado, <strong>${nome}</strong>! Sua mensagem foi enviada com sucesso.`;
-  form.reset();
-});
